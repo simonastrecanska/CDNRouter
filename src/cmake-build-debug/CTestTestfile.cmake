8aa36aa3ea4d805b44c90da073232e2ad1b42c39
Simona Strečanská <simona.strec@gmail.com>
2024-07-29 23:55:24 +0200

    Remove unnecessary files

--- src/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/simonastrecanska/Desktop/cdn
-# Build directory: /Users/simonastrecanska/Desktop/cdn/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/simonastrecanska/Desktop/cdn/cmake-build-debug/RadixTreeTest[1]_include.cmake")
-subdirs("_deps/googletest-build")
